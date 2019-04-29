f0d36519d489ab41e2265093af8b5af63f090801
Jerome Drese <jdrese@gmail.com>
2019-04-29 16:17:20 +0200

    Add MGEAR_CACHE_MANAGER_RIG_ATTRIBUTE value check

--- scripts/mgear/animbits/cache_manager/query.py
+++ scripts/mgear/animbits/cache_manager/query.py
@@ -12,6 +12,25 @@ _MANAGER_CACHE_DESTINATION = os.getenv("MGEAR_CACHE_MANAGER_CACHE_DESTINATION")
 # ==============================================================================
 
 
+def get_cache_destination_path():
+    """ Returns the cache destination path
+
+    This methods returns a path pointing to where the cache manager will store
+    the GPU caches.
+
+    If the **MGEAR_CACHE_MANAGER_CACHE_DESTINATION** environment
+    variable has been set it will return whatever path has been set if valid.
+    If none has been set or fails to use that path this method tries then
+    to return whatever settings has been set on the **cache manager preferences
+    file**.
+
+    Finally if no environment variable or preference file is been set then we
+    use the **OS TEMP** folder as destination path.
+    """
+
+    pass
+
+
 def get_scene_rigs():
     """ The rigs from current Maya session
 
@@ -24,11 +43,17 @@ def get_scene_rigs():
         list: mGear rig top node or None
     """
 
-    if not _MANAGER_RIG_ATTRIBUTE:
+    if _MANAGER_RIG_ATTRIBUTE:
+        try:
+            rigs = [x.split(".")[0] for x in cmds.ls(
+                "*.{}".format(_MANAGER_RIG_ATTRIBUTE), recursive=True)]
+        except RuntimeError:
+            raise ValueError("Invalid attribute key: {} - is not a valid "
+                             "attribute key to set on the "
+                             "MGEAR_CACHE_MANAGER_RIG_ATTRIBUTE variable"
+                             .format(_MANAGER_RIG_ATTRIBUTE))
+    else:
         rigs = [x.split(".")[0] for x in cmds.ls("*.gear_version",
                                                  recursive=True)]
-    else:
-        rigs = [x.split(".")[0] for x in cmds.ls(
-            "*.{}".format(_MANAGER_RIG_ATTRIBUTE), recursive=True)]
 
     return rigs or None

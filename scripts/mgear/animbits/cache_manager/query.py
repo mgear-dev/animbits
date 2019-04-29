0ccb92d488e32a18154000db464da6232b393ba9
Jerome Drese <jdrese@gmail.com>
2019-04-29 15:24:39 +0200

    Add detail docstrings for the get_scene_rigs method

--- scripts/mgear/animbits/cache_manager/query.py
+++ scripts/mgear/animbits/cache_manager/query.py
@@ -15,6 +15,11 @@ _MANAGER_CACHE_DESTINATION = os.getenv("MGEAR_CACHE_MANAGER_CACHE_DESTINATION")
 def get_scene_rigs():
     """ The rigs from current Maya session
 
+    This method search for rigs in your current Maya scene.
+    If the MGEAR_CACHE_MANAGER_RIG_ATTRIBUTE has been set it will try to find
+    rigs based on the attribute set on the environment variable. Otherwise
+    it will use the attribute **gear_version** in order to find rigs in scene.
+
     Returns:
         list: mGear rig top node or None
     """

899f1a9a15f6ba9229bd656ace34a56a2ab946e2
Jerome Drese <jdrese@gmail.com>
2019-04-30 17:59:45 +0200

    Add cache path methods

--- scripts/mgear/animbits/cache_manager/query.py
+++ scripts/mgear/animbits/cache_manager/query.py
@@ -1,6 +1,7 @@
 
 # imports
 import os
+import json
 from maya import cmds
 
 # ==============================================================================
@@ -9,6 +10,8 @@ from maya import cmds
 
 _MANAGER_RIG_ATTRIBUTE = os.getenv("MGEAR_CACHE_MANAGER_RIG_ATTRIBUTE")
 _MANAGER_CACHE_DESTINATION = os.getenv("MGEAR_CACHE_MANAGER_CACHE_DESTINATION")
+_MANAGER_PREFERENCE_PATH = "{}/mGear".format(os.getenv("MAYA_APP_DIR"))
+_MANAGER_PREFERENCE_FILE = "animbits_cache_manager.json"
 # ==============================================================================
 
 
@@ -28,7 +31,41 @@ def get_cache_destination_path():
     use the **OS TEMP** folder as destination path.
     """
 
-    pass
+    # if env variable is set
+    if _MANAGER_CACHE_DESTINATION:
+        return _MANAGER_CACHE_DESTINATION
+
+    # if pref file exists
+    if get_preference_file_cache_destination_path():
+        return get_preference_file_cache_destination_path()
+
+    # returns temp. folder
+    return os.getenv("TMPDIR")
+
+
+def get_preference_file_cache_destination_path():
+    """ Returns the folder path set on the preference file
+
+    Returns:
+        str or None: The path stored in the preference file or None if invalid
+    """
+
+    # preference file
+    _file = "{}/{}".format(_MANAGER_PREFERENCE_PATH, _MANAGER_PREFERENCE_FILE)
+
+    # checks if file exists and its an actual file
+    if not os.path.isfile(_file):
+        return None
+
+    try:
+        with open(_file, 'r') as file_r:
+            json_dict = json.load(file_r)
+            return json_dict["preferences"]["cache_path"]
+    except Exception as issue:
+        message = "Contact mGear's developers reporting this issue to get help"
+        print("{} - {} / {}".format(type(issue).__name__, issue,
+                                    message))
+        return None
 
 
 def get_scene_rigs():
@@ -40,7 +77,7 @@ def get_scene_rigs():
     it will use the attribute **gear_version** in order to find rigs in scene.
 
     Returns:
-        list: mGear rig top node or None
+        list or None: mGear rig top node or None
     """
 
     if _MANAGER_RIG_ATTRIBUTE:

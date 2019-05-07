def8e09eec958f71db514afac71d36c63bb12f31
Jerome Drese <jdrese@gmail.com>
2019-05-07 10:49:24 +0200

    reorder constants (pep8)

--- scripts/mgear/animbits/cache_manager/query.py
+++ scripts/mgear/animbits/cache_manager/query.py
@@ -9,10 +9,11 @@ from maya import cmds
 # CONSTANTS
 # ==============================================================================
 
-_MANAGER_RIG_ATTRIBUTE = os.getenv("MGEAR_CACHE_MANAGER_RIG_ATTRIBUTE")
 _MANAGER_CACHE_DESTINATION = os.getenv("MGEAR_CACHE_MANAGER_CACHE_DESTINATION")
-_MANAGER_PREFERENCE_PATH = "{}/mGear".format(os.getenv("MAYA_APP_DIR"))
+_MANAGER_MODEL_GROUP = os.getenv("MGEAR_CACHE_MANAGER_MODEL_GROUP")
 _MANAGER_PREFERENCE_FILE = "animbits_cache_manager.json"
+_MANAGER_PREFERENCE_PATH = "{}/mGear".format(os.getenv("MAYA_APP_DIR"))
+_MANAGER_RIG_ATTRIBUTE = os.getenv("MGEAR_CACHE_MANAGER_RIG_ATTRIBUTE")
 # ==============================================================================
 
 
@@ -43,6 +44,25 @@ def get_cache_destination_path():
 
     # returns temp. folder
     return os.getenv("TMPDIR")
+
+
+def get_model_group():
+    """ Returns the model group name to cache
+
+    This methods returns a string with the name of the transform node to use
+    when caching the geometry/model
+
+    If the **MGEAR_CACHE_MANAGER_MODEL_GROUP** environment
+    variable has been set it will return the name stored in it.
+    If none has been set or fails to use that name this method tries then
+    to return whatever settings has been set on the **cache manager preferences
+    file**.
+
+    Finally if no environment variable or preference file is been set then we
+    fall back to selection.
+    """
+
+    pass
 
 
 def get_preference_file_cache_destination_path():

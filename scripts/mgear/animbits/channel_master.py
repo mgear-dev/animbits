48f7ff35b05ec467f4a4bc931482aded77345fa5
miquelcampos <miquel.campos@gmail.com>
2020-03-03 19:14:46 +0900

    channel master WIP #17

--- scripts/mgear/animbits/channel_master.py
+++ scripts/mgear/animbits/channel_master.py
@@ -1,8 +1,11 @@
 import maya.cmds as cmds
 import pymel.core as pm
 from mgear.core import pyflow_widgets
-from mgear.vendor.Qt import QtGui, QtCore, QtWidgets
+from mgear.core import pyqt
+from mgear.vendor.Qt import QtCore, QtWidgets
 import timeit
+from functools import partial
+from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
 
 
 DEFAULT_RANGE = 1000
@@ -79,6 +82,97 @@ def refresh_channel_value():
     pass
 
 
+class ChannelMaster(MayaQWidgetDockableMixin, QtWidgets.QDialog):
+
+    def __init__(self, attrs, parent=None):
+        super(ChannelMaster, self).__init__(parent)
+        self.deleteLater = True
+        self.attrs = attrs
+
+        self.setWindowTitle("Channel Master")
+        self.setMinimumWidth(150)
+        flags = self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint
+        self.setWindowFlags(flags)
+
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose, 1)
+
+        self.create_widgets()
+        self.create_layout()
+
+    def create_widgets(self):
+        pass
+
+    def create_layout(self):
+
+        table_style = """
+        QTableView {
+           border: 0 solid transparent;
+        }
+
+        """
+
+        main_layout = QtWidgets.QVBoxLayout(self)
+        main_table = QtWidgets.QTableWidget(self)
+        main_table.setColumnCount(3)
+        main_table.verticalHeader().setVisible(False)
+        main_table.horizontalHeader().setVisible(False)
+        header_view = main_table.horizontalHeader()
+        main_table.setStyleSheet(table_style)
+
+        header_view.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
+        header_view.resizeSection(0,80)
+        header_view.resizeSection(1,17)
+
+        i =0
+        for k in attrs["_attrs"]:
+            at = attrs[k]
+            if at["type"] in ATTR_SLIDER_TYPES:
+                val = cmds.getAttr(ctl + "." + k)
+                sld = pyflow_widgets.pyf_Slider(self,
+                                                defaultValue=val,
+                                                sliderRange=(at["min"],
+                                                             at["max"]))
+
+                label = QtWidgets.QLabel(at["niceName"] + "  ")
+                label.setMinimumWidth(80)
+                label.setMaximumWidth(80)
+                label.setToolTip(at["longName"])
+                label.setAlignment(QtCore.Qt.AlignBottom
+                                   | QtCore.Qt.AlignRight)
+
+                button = QtWidgets.QPushButton()
+                button.setMaximumHeight(17)
+                button.setMinimumHeight(17)
+                button.setMaximumWidth(17)
+                button.setMinimumWidth(17)
+                button.setIcon( pyqt.get_icon("unlock", 11))
+                button.setCheckable(True)
+
+                main_table.insertRow(i)
+                main_table.setRowHeight(i, 17)
+                main_table.setCellWidget(i, 0, label)
+                main_table.setCellWidget(i, 1, button)
+                main_table.setCellWidget(i, 2, sld)
+                i+=1
+
+
+                button.clicked.connect(partial(self.changeIcon, button))
+
+
+        main_layout.addWidget(main_table)
+        # main_layout.addStretch()
+
+        main_layout.setContentsMargins(2, 2, 2, 2)
+        main_layout.setSpacing(0)
+
+    def changeIcon(self, button):
+        if button.isChecked():
+            print "down"
+            button.setIcon(pyqt.get_icon("lock", 11))
+        else:
+            print "up"
+            button.setIcon(pyqt.get_icon("unlock", 11))
+
 if __name__ == "__main__":
 
     start = timeit.default_timer()
@@ -86,94 +180,11 @@ if __name__ == "__main__":
     ctl = pm.selected()[0].name()
     attrs = get_attributes_config(ctl)
 
-    class slider_dialog(QtWidgets.QDialog):
 
-        def __init__(self, attrs, parent=None):
-            super(slider_dialog, self).__init__(parent)
-            self.deleteLater = True
-            self.attrs = attrs
 
-            self.setWindowTitle("slider test dialog")
-            self.setMinimumWidth(200)
-            flags = self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint
-            self.setWindowFlags(flags)
+    # d = ChannelMaster(attrs)
 
-            self.create_widgets()
-            self.create_layout()
-
-        def create_widgets(self):
-            pass
-
-        def create_layout(self):
-
-            table_style = """
-            QLabel{
-                background: transparent;
-                border: 0 solid transparent;
-                color: #c8c8c8;
-            }
-            QTableView {
-               border: 0 solid transparent;
-            }
-
-            """
-
-            main_layout = QtWidgets.QVBoxLayout(self)
-            main_table = QtWidgets.QTableWidget(self)
-            main_table.setColumnCount(3)
-            main_table.verticalHeader().setVisible(False)
-            main_table.horizontalHeader().setVisible(False)
-            header_view = main_table.horizontalHeader()
-            main_table.setStyleSheet(table_style)
-
-            header_view.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
-            header_view.resizeSection(0,80)
-            header_view.resizeSection(1,17)
-            # main_table.setRowCount(20)
-
-            i =0
-            for k in attrs["_attrs"]:
-                at = attrs[k]
-                if at["type"] in ATTR_SLIDER_TYPES:
-                    # channel_layout = QtWidgets.QHBoxLayout(self)
-                    val = cmds.getAttr(ctl + "." + k)
-                    sld = pyflow_widgets.pyf_Slider(self,
-                                                    defaultValue=val,
-                                                    sliderRange=(at["min"],
-                                                                 at["max"]))
-
-                    label = QtWidgets.QLabel(at["niceName"] + "  ")
-                    label.setMinimumWidth(80)
-                    label.setMaximumWidth(80)
-                    label.setToolTip(at["longName"])
-                    label.setAlignment(QtCore.Qt.AlignBottom
-                                       | QtCore.Qt.AlignRight)
-
-                    button = QtWidgets.QPushButton()
-                    button.setMaximumHeight(17)
-                    button.setMinimumHeight(17)
-                    button.setMaximumWidth(17)
-                    button.setMinimumWidth(17)
-                    # channel_layout.addWidget(label)
-                    # channel_layout.addWidget(button)
-                    # channel_layout.addWidget(sld)
-                    # channel_layout.setContentsMargins(0, 0, 0, 0)
-
-                    main_table.insertRow(i)
-                    main_table.setRowHeight(i, 17)
-                    main_table.setCellWidget(i, 0, label)
-                    main_table.setCellWidget(i, 1, button)
-                    main_table.setCellWidget(i, 2, sld)
-                    i+=1
-            main_layout.addWidget(main_table)
-            # main_layout.addStretch()
-
-            main_layout.setContentsMargins(2, 2, 2, 2)
-            main_layout.setSpacing(0)
-
-    d = slider_dialog(attrs)
-
-    d.show()
+    pyqt.showDialog(partial(ChannelMaster, attrs), dockable=True)
 
     end = timeit.default_timer()
     timeConsumed = end - start
